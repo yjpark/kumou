479fd01904c32e0c403ca1a71119ff084260267e
YJ Park <yjpark@gmail.com>
2024-04-17 14:31:56 +0800

    🚧 [WIP] - 2024-04-17 14:31:56 -

--- app/classes.rs
+++ app/classes.rs
@@ -1,41 +1,351 @@
 vec![
 
-/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(113629) }
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211284) }
 w_screen h_screen flex
  */
 Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
 
-/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(113634) }
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211289) }
 w_full
  */
 Class :: from (vec ! [String :: from (w_full)]),
 
-/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(113651) }
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211317) }
 w_screen h_screen flex
  */
 Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
 
-/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(113656) }
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211322) }
 w_full
  */
 Class :: from (vec ! [String :: from (w_full)]),
 
-/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(113661) }
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211284) }
 w_screen h_screen flex
  */
 Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
 
-/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(113666) }
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211289) }
 w_full
  */
 Class :: from (vec ! [String :: from (w_full)]),
 
-/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(113671) }
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211317) }
 w_screen h_screen flex
  */
 Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
 
-/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(113676) }
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211322) }
+w_full
+ */
+Class :: from (vec ! [String :: from (w_full)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211284) }
+w_screen h_screen flex
+ */
+Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211289) }
+w_full
+ */
+Class :: from (vec ! [String :: from (w_full)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211317) }
+w_screen h_screen flex
+ */
+Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211322) }
+w_full
+ */
+Class :: from (vec ! [String :: from (w_full)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211284) }
+w_screen h_screen flex
+ */
+Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211289) }
+w_full
+ */
+Class :: from (vec ! [String :: from (w_full)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211317) }
+w_screen h_screen flex
+ */
+Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211322) }
+w_full
+ */
+Class :: from (vec ! [String :: from (w_full)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211284) }
+w_screen h_screen flex
+ */
+Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211289) }
+w_full
+ */
+Class :: from (vec ! [String :: from (w_full)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211317) }
+w_screen h_screen flex
+ */
+Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211322) }
+w_full
+ */
+Class :: from (vec ! [String :: from (w_full)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211284) }
+w_screen h_screen flex
+ */
+Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211289) }
+w_full
+ */
+Class :: from (vec ! [String :: from (w_full)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211284) }
+w_screen h_screen flex
+ */
+Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211289) }
+w_full
+ */
+Class :: from (vec ! [String :: from (w_full)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211317) }
+w_screen h_screen flex
+ */
+Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211322) }
+w_full
+ */
+Class :: from (vec ! [String :: from (w_full)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211284) }
+w_screen h_screen flex
+ */
+Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211289) }
+w_full
+ */
+Class :: from (vec ! [String :: from (w_full)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211317) }
+w_screen h_screen flex
+ */
+Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211322) }
+w_full
+ */
+Class :: from (vec ! [String :: from (w_full)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211284) }
+w_screen h_screen flex
+ */
+Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211289) }
+w_full
+ */
+Class :: from (vec ! [String :: from (w_full)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211317) }
+w_screen h_screen flex
+ */
+Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211322) }
+w_full
+ */
+Class :: from (vec ! [String :: from (w_full)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(212320) }
+w_screen h_screen flex
+ */
+Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(212325) }
+w_full
+ */
+Class :: from (vec ! [String :: from (w_full)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(212341) }
+w_screen h_screen flex
+ */
+Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(212346) }
+w_full
+ */
+Class :: from (vec ! [String :: from (w_full)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(212320) }
+w_screen h_screen flex
+ */
+Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(212325) }
+w_full
+ */
+Class :: from (vec ! [String :: from (w_full)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(212341) }
+w_screen h_screen flex
+ */
+Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(212346) }
+w_full
+ */
+Class :: from (vec ! [String :: from (w_full)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211284) }
+w_screen h_screen flex
+ */
+Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211289) }
+w_full
+ */
+Class :: from (vec ! [String :: from (w_full)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211317) }
+w_screen h_screen flex
+ */
+Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211322) }
+w_full
+ */
+Class :: from (vec ! [String :: from (w_full)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211284) }
+w_screen h_screen flex
+ */
+Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211289) }
+w_full
+ */
+Class :: from (vec ! [String :: from (w_full)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211317) }
+w_screen h_screen flex
+ */
+Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211322) }
+w_full
+ */
+Class :: from (vec ! [String :: from (w_full)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211284) }
+w_screen h_screen flex
+ */
+Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211289) }
+w_full
+ */
+Class :: from (vec ! [String :: from (w_full)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211317) }
+w_screen h_screen flex
+ */
+Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211322) }
+w_full
+ */
+Class :: from (vec ! [String :: from (w_full)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211284) }
+w_screen h_screen flex
+ */
+Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211289) }
+w_full
+ */
+Class :: from (vec ! [String :: from (w_full)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211317) }
+w_screen h_screen flex
+ */
+Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211322) }
+w_full
+ */
+Class :: from (vec ! [String :: from (w_full)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211284) }
+w_screen h_screen flex
+ */
+Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211289) }
+w_full
+ */
+Class :: from (vec ! [String :: from (w_full)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211317) }
+w_screen h_screen flex
+ */
+Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211322) }
+w_full
+ */
+Class :: from (vec ! [String :: from (w_full)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211284) }
+w_screen h_screen flex
+ */
+Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211289) }
+w_full
+ */
+Class :: from (vec ! [String :: from (w_full)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211317) }
+w_screen h_screen flex
+ */
+Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211322) }
+w_full
+ */
+Class :: from (vec ! [String :: from (w_full)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211284) }
+w_screen h_screen flex
+ */
+Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211289) }
+w_full
+ */
+Class :: from (vec ! [String :: from (w_full)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211317) }
+w_screen h_screen flex
+ */
+Class :: from (vec ! [String :: from (w_screen) , String :: from (h_screen) , String :: from (flex)]),
+
+/* SpanData { range: 435..440, anchor: SpanAnchor(FileId(515), 7), ctx: SyntaxContextId(211322) }
 w_full
  */
 Class :: from (vec ! [String :: from (w_full)]),
--- app/css/classes.rs
+++ app/css/classes.rs
@@ -1,13 +1,13 @@
 vec![
 
-/* #229 bytes(269..299)
+/* #229 bytes(2475..2505)
 class!(w_screen h_screen flex)
  */
 Class ::
 from(vec!
 [String :: from(w_screen), String :: from(h_screen), String :: from(flex)]),
 
-/* #235 bytes(342..356)
+/* #235 bytes(2548..2562)
 class!(w_full)
  */
 Class :: from(vec! [String :: from(w_full)]),
